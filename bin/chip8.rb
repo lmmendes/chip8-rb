ae090996614e48cedc3ab92fe1c089478d01d6db
Luis Mendes <luis.mendes@daimler.com>
2017-10-01 21:56:57 +0100

    Initial support for source code dumping

---
+++ bin/chip8.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require "bundler/setup"
+require "chip8"
+require "optparse"
+
+options = {}
+optparse = OptionParser.new do |opts|
+  opts.banner = "Usage: chip8 file.rom [options]"
+  opts.on("-d", "--dump", "Run verbosely") do
+    options[:dump] = true
+  end
+end
+
+optparse.parse!
+
+filename = ARGV.pop
+
+if !filename
+  puts optparse
+  exit
+end
+
+if options[:dump]
+  Chip8::Dump.new("/Users/lmmendes/Downloads/c8games/Chip8Test").dump
+  exit
+end
+
+#Chip8::Dump.new("/Users/lmmendes/Downloads/c8games/Chip8Test").dump
---
+++ lib/chip8/dump.rb
@@ -0,0 +1,94 @@
+module Chip8
+  class Dump
+    attr_reader :file, :rom
+
+    def initialize(file)
+      @file = file
+      @rom  = File.open(@file, "rb").read.unpack("n*")
+    end
+
+    def dump
+      puts ""
+      puts "File #{file} has #{rom.size} instructions."
+      puts "For information about instructions see "
+      puts "http://devernay.free.fr/hacks/chip8/chip8def.htm"
+      puts ""
+      @rom.each_with_index do |opcode, addr|
+        hex = "%04X" % opcode
+        puts sprintf "0x%03X: [%s] %s", 200+addr*2, hex, decode(opcode)
+      end
+      puts ""
+    end
+
+    def decode(opcode)
+      o   = opcode >> 12
+      kk  = opcode & 0x0FF
+      nnn = opcode & 0XFFF
+      x   = opcode >> 8 & 0x0F
+      y   = opcode >> 4 & 0x0F
+      l   = opcode & 0x0F
+
+      case o
+      when 0
+        "NOP"
+      when 6
+        case kk
+        when 0x00E0
+          "CLS"
+        when 0x00EE
+          "RTS"
+        else
+          sprintf "MOV V%s, 0x%X", x, kk
+        end
+      when 1
+        sprintf "JMP %03X", nnn
+      when 2
+        sprintf "JSR %03X", nnn
+      when 3
+        "SKEQ V#{x}, #{kk}"
+      when 4
+        "SKNE V#{x}, #{kk}"
+      when 5
+        "SKNE V#{x}, V#{y}"
+      when 6
+        "MOV V#{x}, V#{y}"
+      when 7
+        "ADD V#{x}, #{y}"
+      when 8
+        case l
+        when 0
+          "MOV V#{x}, V#{y}"
+        when 1
+          "OR  V#{x}, V#{y}"
+        when 2
+          "AND  V#{x}, V#{y}"
+        when 3
+          "XOR  V#{x}, V#{y}"
+        when 4
+          "ADD  V#{x}, V#{y}"
+        when 5
+          "SUB  V#{x}, V#{y}"
+        when 6
+          "SHR  V#{x}, V#{y}"
+        when 7
+          "RSB  V#{x}, V#{y}"
+        when 0xE
+          "SHL V#{x}"
+        end
+      when 9
+        "SKNE V#{x}, V#{y}"
+      when 0xA
+        "MVI #{nnn}"
+      when 0xB
+        "JMI #{nnn}"
+      when 0xC
+        "RAND V#{x}, #{kk}"
+      when 0xD
+        "SPRITE V#{x}, #{kk}, #{n}"
+      end
+
+    end
+
+
+  end
+end
